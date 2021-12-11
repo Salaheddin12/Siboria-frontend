5a4771fa9a5f3adf1ee748c06536bee9bc1d5e1b
Salah Eddine Ait Blakacem <ab.salah012@gmail.com>
2021-12-11 13:07:35 +0100

    fix: fix newsletter spacing

--- components/sections/NewsLetter/index.tsx
+++ components/sections/NewsLetter/index.tsx
@@ -13,13 +13,13 @@ import { FiSend } from "react-icons/fi";
 
 export default function CardWithIllustration() {
   return (
-    <Flex minH={"100vh"} align={"center"} justify={"center"} py={12}>
+    <Flex minH={"50vh"} align={"center"} justify={"center"} py={12}>
       <Stack
         boxShadow={"2xl"}
         bg={"darkBlue"}
         rounded={"xl"}
-        py={16}
-        px={20}
+        py={{ base: 4, md: 12, lg: 16 }}
+        px={{ base: 4, md: 16, lg: 20 }}
         width={"100%"}
         direction={"row"}
         spacing={8}
@@ -70,7 +70,7 @@ export default function CardWithIllustration() {
             </Button>
           </Stack>
         </Flex>
-        <Box>
+        <Box display={{ base: "none", md: "block" }}>
           <Image alt="pot" src="/images/pot.png" width={350} height={350} />
         </Box>
       </Stack>
